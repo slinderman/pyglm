d73b60cd6b5fc69ee32017fdaef578a51359a864
scott <scott.linderman@gmail.com>
2015-10-01 23:44:54 -0400

    adding block inverse for adding and removing rows

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -1,7 +1,9 @@
 import numpy as np
 import scipy.linalg
 
-from pyglm.utils.cholesky import chol_update, chol_downdate, chol_remove_row, chol_add_row
+from pyglm.utils.linalg import \
+    chol_update, chol_downdate, chol_remove_row, chol_add_row, \
+    block_inverse_add_rows, block_inverse_remove_rows
 
 # def test_chol_update():
 #     N = 5
@@ -17,7 +19,6 @@ from pyglm.utils.cholesky import chol_update, chol_downdate, chol_remove_row, ch
 #     assert np.allclose(Uhat.T.dot(Uhat), Utrue.T.dot(Utrue))
 
 def test_chol_update():
-
     A = np.random.randn(3,3)
     A = A.dot(A.T)
     A += 4*np.eye(3)
@@ -71,7 +72,48 @@ def test_chol_add_remove():
                                     to_range(1,3))]),
                            chol_remove_row(L,1,3))
 
+def test_block_inverse_add_row():
+    N = 4
+    X = np.random.randn(100,N)
+    A = X.T.dot(X)
+    # A = np.random.randn(4,4)
+    # A = A.dot(A.T)
+    # A += 4*np.eye(4)
+    Ainv = np.linalg.inv(A)
+
+    # Get a subset of A and add rows
+    end = 3
+    Bm = A[:end,end:]
+    Cm = Bm.T
+    Dm = A[end:,end:]
+
+    Pt,Qt,Rt,St = block_inverse_add_rows(Ainv[:end,:end], Bm, Cm, Dm, symm=True)
+    assert np.allclose(Ainv[:end,:end], Pt, atol=1e-3)
+    assert np.allclose(Ainv[:end,end:], Qt, atol=1e-3)
+    assert np.allclose(Ainv[end:,:end], Rt, atol=1e-3)
+    assert np.allclose(Ainv[end:,end:], St, atol=1e-3)
+
+def test_block_inverse_remove_row():
+    N = 4
+    X = np.random.randn(100,N)
+    A = X.T.dot(X)
+    # A = np.random.randn(4,4)
+    # A = A.dot(A.T)
+    # A += 4*np.eye(4)
+    Ainv = np.linalg.inv(A)
+
+    # Get a subset of A and add rows
+    for end in xrange(1,4):
+        Am = A[:end,:end]
+        Pmt = np.linalg.inv(Am)
+
+        Pmt_tilde = block_inverse_remove_rows(Ainv, end, symm=True)
+        assert np.allclose(Pmt, Pmt_tilde, atol=1e-3)
+
+
 if __name__ == "__main__":
     # test_chol_add_remove()
-    test_chol_update()
-    test_chol_downdate()
+    # test_chol_update()
+    # test_chol_downdate()
+    test_block_inverse_add_row()
+    test_block_inverse_remove_row()
